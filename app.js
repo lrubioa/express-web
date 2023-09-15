01e25531116367c94860951e7d8c688ed68da025
lrubioa <111470342+lrubioa@users.noreply.github.com>
2023-09-15 13:05:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('./public/images'))
+// app.engine('ejs', require('ejs'))
+
+app.set('views', './views');
+// app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {text: 'This is EJS'});
+});
+
+app.get('/showcase', (req, res) => {
+    res.render('showcase', {text: 'This is EJS'});
+});
+
+app.get('/services', (req, res) => {
+    res.render('services', {text: 'This is EJS'});
+});
+
+app.get('/designers', (req, res) => {
+    res.render('designers', {text: 'This is EJS'});
+});
+
+app.get('/package', (req, res) => {
+    res.render('package', {text: 'This is EJS'});
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {text: 'This is EJS'});
+});
+
+
+
+app.listen(port, () =>{
+    console.log('Server/App is running at', port);
+});
